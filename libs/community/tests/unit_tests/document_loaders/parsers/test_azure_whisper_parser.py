ff4763a0957956074314391027f1000317546e5e
Sheepsta300 <128811766+Sheepsta300@users.noreply.github.com>
2024-09-18 19:29:42 +1200

    change parameter name to be correct

--- libs/community/tests/unit_tests/document_loaders/parsers/test_azure_whisper_parser.py
+++ libs/community/tests/unit_tests/document_loaders/parsers/test_azure_whisper_parser.py
@@ -16,7 +16,7 @@ def test_azure_openai_whisper(mock_client: MagicMock) -> None:
     version = "37"
 
     parser = AzureOpenAIWhisperParser(
-        api_endpoint=endpoint, api_key=key, deployment_name=name, api_version=version
+        api_key=key, azure_endpoint=endpoint, deployment_name=name, api_version=version
     )
     mock_client.assert_called_once_with(
         endpoint=endpoint, key=key, deployment_name=name, version=version
