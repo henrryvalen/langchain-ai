ce9bd8ce6bd3e3c69af343fbc7471f2ca51b0268
Sheepsta300 <128811766+Sheepsta300@users.noreply.github.com>
2024-09-18 19:26:29 +1200

    add attempt at unit test

---
+++ libs/community/tests/unit_tests/document_loaders/parsers/test_azure_whisper_parser.py
@@ -0,0 +1,24 @@
+"""Tests for the Azure OpenAI Whisper parser."""
+
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from langchain_community.document_loaders.parsers.audio import AzureOpenAIWhisperParser
+
+
+@pytest.mark.requires("openai")
+@patch("openai.AzureOpenAI")
+def test_azure_openai_whisper(mock_client: MagicMock) -> None:
+    endpoint = "endpoint"
+    key = "key"
+    name = "model"
+    version = "37"
+
+    parser = AzureOpenAIWhisperParser(
+        api_endpoint=endpoint, api_key=key, deployment_name=name, api_version=version
+    )
+    mock_client.assert_called_once_with(
+        endpoint=endpoint, key=key, deployment_name=name, version=version
+    )
+    assert parser.client == mock_client()
