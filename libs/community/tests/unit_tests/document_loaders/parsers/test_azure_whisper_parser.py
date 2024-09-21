458348826e8ccd6c5f31556b73718c81a83ff26e
Sheepsta300 <128811766+Sheepsta300@users.noreply.github.com>
2024-09-22 10:48:13 +1200

    add working unit tests for parser and test file

--- libs/community/tests/unit_tests/document_loaders/parsers/test_azure_whisper_parser.py
+++ libs/community/tests/unit_tests/document_loaders/parsers/test_azure_whisper_parser.py
@@ -1,17 +1,23 @@
 """Tests for the Azure OpenAI Whisper parser."""
 
-import io
-from unittest.mock import MagicMock, patch
+from pathlib import Path
+from unittest.mock import Mock, patch
 
 import pytest
+from langchain_core.documents import Document
 from langchain_core.documents.base import Blob
 
 from langchain_community.document_loaders.parsers.audio import AzureOpenAIWhisperParser
 
+_THIS_DIR = Path(__file__).parents[3]
+
+_EXAMPLES_DIR = _THIS_DIR / "examples"
+AUDIO_M4A = _EXAMPLES_DIR / "hello_world.m4a"
+
 
 @pytest.mark.requires("openai")
 @patch("openai.AzureOpenAI")
-def test_azure_openai_whisper(mock_client: MagicMock) -> None:
+def test_azure_openai_whisper(mock_client: Mock) -> None:
     endpoint = "endpoint"
     key = "key"
     version = "115"
@@ -31,41 +37,67 @@ def test_azure_openai_whisper(mock_client: MagicMock) -> None:
 
 
 @pytest.mark.requires("openai")
-@patch("openai.AzureOpenAI")
-@patch("openai.AzureOpenAI.audio.transcriptions.create")
-def test_azure_openai_whisper_lazy_parse(
-    mock_transcribe: MagicMock, mock_client: MagicMock
-) -> None:
+def test_is_openai_v1_lazy_parse(mocker):
     endpoint = "endpoint"
     key = "key"
     version = "115"
     name = "model"
 
-    mock_client.return_value = mock_client
+    mock_blob = mocker.Mock(spec=Blob)
+    mock_blob.path = AUDIO_M4A
+    mock_blob.source = "test_source"
+
+    mock_openai_client = mocker.Mock()
+
+    mock_openai_client.audio.transcriptions.create.return_value = mocker.Mock()
+    mock_openai_client.audio.transcriptions.create.return_value.text = (
+        "Transcribed text"
+    )
+
+    mocker.patch("langchain_community.utils.openai.is_openai_v1", return_value=True)
 
     parser = AzureOpenAIWhisperParser(
         api_key=key, azure_endpoint=endpoint, api_version=version, deployment_name=name
     )
 
-    mock_client.assert_called_once_with(
-        api_key=key,
-        azure_endpoint=endpoint,
-        api_version=version,
-        max_retries=3,
-        azure_ad_token=None,
+    parser._client = mock_openai_client
+
+    result = list(parser.lazy_parse(mock_blob))
+
+    assert len(result) == 1
+    assert isinstance(result[0], Document)
+    assert result[0].page_content == "Transcribed text"
+    assert result[0].metadata["source"] == "test_source"
+
+
+@pytest.mark.requires("openai")
+def test_is_not_openai_v1_lazy_parse(mocker):
+    endpoint = "endpoint"
+    key = "key"
+    version = "115"
+    name = "model"
+
+    mock_blob = mocker.Mock(spec=Blob)
+    mock_blob.path = AUDIO_M4A
+    mock_blob.source = "test_source"
+
+    mock_openai_client = mocker.Mock()
+
+    mock_openai_client.audio.transcriptions.create.return_value = mocker.Mock()
+    mock_openai_client.audio.transcriptions.create.return_value.text = (
+        "Transcribed text"
     )
 
-    mock_response = MagicMock()
-    mock_response.text = "This is a mock transcription"
-    mock_transcribe.return_value = mock_response
+    mocker.patch("langchain_community.utils.openai.is_openai_v1", return_value=False)
 
-    blob = Blob(path="audio_path.m4a", data=b"Great day for fishing ain't it")
-    docs = parser.lazy_parse(blob=blob)
-
-    file_obj = io.BytesIO(b"Great day for fishing ain't it")
-    mock_transcribe.assert_called_once_with(
-        model=name,
-        file=file_obj,
+    parser = AzureOpenAIWhisperParser(
+        api_key=key, azure_endpoint=endpoint, api_version=version, deployment_name=name
     )
-    for doc in docs:
-        assert doc.page_content == mock_response.text
+    parser._client = mock_openai_client
+
+    result = list(parser.lazy_parse(mock_blob))
+
+    assert len(result) == 1
+    assert isinstance(result[0], Document)
+    assert result[0].page_content == "Transcribed text"
+    assert result[0].metadata["source"] == "test_source"
